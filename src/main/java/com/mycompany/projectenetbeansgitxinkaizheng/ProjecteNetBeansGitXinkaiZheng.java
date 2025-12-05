a910ac54839ca8ee633ae1c2d4b21b680c8a726a
xinkai <kinzhe@jviladoms.cat>
2025-12-05 21:59:31 +0100

    Primer Commit

---
+++ src/main/java/com/mycompany/projectenetbeansgitxinkaizheng/ProjecteNetBeansGitXinkaiZheng.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectenetbeansgitxinkaizheng;
+
+/**
+ *
+ * @author lucas
+ */
+public class ProjecteNetBeansGitXinkaiZheng {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
